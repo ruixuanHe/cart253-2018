89b8b728edfa9b2639fc4f72f21f0ed6c353c2db
ruixuanHe <ruixuanheweb@gmail.com>
2018-09-20 15:59:13 -0400

    add exercise2

--- exercises/template-p5-project/js/script.js
+++ exercises/template-p5-project/js/script.js
@@ -11,21 +11,16 @@ author, and this description to match your project!
 // setup()
 //
 // Description of setup
-
-var x;
-var y;
-var speed = 10;
-var vx;
-var ax = 0.1;
+var x = 0;
 function setup() {
   createCanvas(500,500);
-  x = 0;
-  y = height/2;
-  vx = 0;
 }
-
 function draw() {
-  vx = constrain(vx+ax,-speed,speed);
-  x = x + vx;
-  ellipse(x,y,50,50);
+}
+function mousePressed() {
+  // Increase x by 1 half the time
+  if (random() < 0.5) {
+    x = x + 1;
+    console.log("x increased to " + x + "!");
+  }
 }
