97a917b0af7bd53f90c76230c744bbc821ad6a0f
ruixuanHe <ruixuanheweb@gmail.com>
2018-11-08 20:24:07 -0500

    P2: improve the game
    improve the game as exercise 5:
    1. change the speed and size of ball
    2. display the score
    3.change paddle size and shape

--- exercises/project2/js/Ball.js
+++ exercises/project2/js/Ball.js
@@ -7,54 +7,67 @@
 // Ball constructor
 //
 // Sets the properties with the provided arguments
-function Ball(x,y,vx,vy,size,speed) {
+///////// NEW /////////
+//challenge: add properties winningSide
+function Ball(x, y, vx, vy, size, speed) {
   this.x = x;
   this.y = y;
   this.vx = vx;
   this.vy = vy;
   this.size = size;
   this.speed = speed;
+  this.winningSide = null;
 }
+///////// END NEW /////////
 
 // update()
 //
 // Moves according to velocity, constrains y to be on screen,
 // checks for bouncing on upper or lower edgs, checks for going
 // off left or right side.
-Ball.prototype.update = function () {
+Ball.prototype.update = function() {
   // Update position with velocity
   this.x += this.vx;
   this.y += this.vy;
 
   // Constrain y position to be on screen
-  this.y = constrain(this.y,0,height-this.size);
+  this.y = constrain(this.y, 0, height);
 
   // Check for touching upper or lower edge and reverse velocity if so
-  if (this.y === 0 || this.y + this.size === height) {
-    this.vy = -this.vy;
-  }
+  if (this.y - this.size / 2 < 0) this.vy = -this.vy;
+  if (this.y + this.size / 2 > height) this.vy = -this.vy;
 }
 
 // isOffScreen()
 //
 // Checks if the ball has moved off the screen and, if so, returns true.
 // Otherwise it returns false.
-Ball.prototype.isOffScreen = function () {
-  // Check for going off screen and reset if so
-  if (this.x + this.size < 0 || this.x > width) {
+///////// NEW /////////
+// change the if condition, if the ball has moved off the right side of screen,
+// it will update winning side to rightPaddle, same for the left side.
+Ball.prototype.isOffScreen = function() {
+  // Check for going off the left side of creen and reset if so
+  if (this.x + this.size / 2 < 0) {
+    this.winningSide = "left";
     return true;
   }
-  else {
+  /// Check for going off the right side of creen and reset if so
+  else if (this.x - this.size / 2 > width) {
+    this.winningSide = "right";
+    return true;
+  } else {
     return false;
   }
+
 }
+///////// END NEW /////////
 
 // display()
 //
 // Draw the ball as a rectangle on the screen
-Ball.prototype.display = function () {
+Ball.prototype.display = function() {
   fill(255);
-  rect(this.x,this.y,this.size,this.size);
+  rect(this.x, this.y, this.size, this.size);
 }
 
 // handleCollision(paddle)
@@ -63,9 +76,9 @@ Ball.prototype.display = function () {
 // and if so reverse x velocity to bounce
 Ball.prototype.handleCollision = function(paddle) {
   // Check if the ball overlaps the paddle on x axis
-  if (this.x + this.size > paddle.x && this.x < paddle.x + paddle.w) {
+  if (this.x + this.size / 2 > (paddle.x - paddle.w / 2) && this.x < paddle.x + paddle.w / 2) {
     // Check if the ball overlaps the paddle on y axis
-    if (this.y + this.size > paddle.y && this.y < paddle.y + paddle.h) {
+    if (this.y + this.size / 2 > (paddle.y - paddle.h / 2) && this.y < paddle.y + paddle.h / 2) {
       // If so, move ball back to previous position (by subtracting current velocity)
       this.x -= this.vx;
       this.y -= this.vy;
@@ -78,7 +91,13 @@ Ball.prototype.handleCollision = function(paddle) {
 // reset()
 //
 // Set position back to the middle of the screen
-Ball.prototype.reset = function () {
-  this.x = width/2;
-  this.y = height/2;
+///////// NEW /////////
+// modify reset method() ball toward the paddle that won the most recent point
+// with random Y velocity
+Ball.prototype.reset = function() {
+  this.x = width / 2;
+  this.y = height / 2;
+  this.vx = -this.vx;
+  this.vy = map(random(0, 1), 0, 1, -this.speed, this.speed);
 }
+///////// END NEW /////////
--- exercises/project2/js/Paddle.js
+++ exercises/project2/js/Paddle.js
@@ -6,7 +6,8 @@
 // Paddle constructor
 //
 // Sets the properties with the provided arguments or defaults
-function Paddle(x,y,w,h,speed,downKey,upKey) {
+///////// NEW /////////
+function Paddle(x, y, w, h, speed, downKey, upKey, paddleSide) {
   this.x = x;
   this.y = y;
   this.vx = 0;
@@ -16,7 +17,11 @@ function Paddle(x,y,w,h,speed,downKey,upKey) {
   this.speed = speed;
   this.downKey = downKey;
   this.upKey = upKey;
+  // add two variables of paddle
+  this.score = 0;
+  this.paddleSide = paddleSide;
 }
+///////// END NEW /////////
 
 // handleInput()
 //
@@ -25,11 +30,9 @@ function Paddle(x,y,w,h,speed,downKey,upKey) {
 Paddle.prototype.handleInput = function() {
   if (keyIsDown(this.upKey)) {
     this.vy = -this.speed;
-  }
-  else if (keyIsDown(this.downKey)) {
+  } else if (keyIsDown(this.downKey)) {
     this.vy = this.speed;
-  }
-  else {
+  } else {
     this.vy = 0;
   }
 }
@@ -39,13 +42,47 @@ Paddle.prototype.handleInput = function() {
 // Constrain the resulting position to be within the canvas
 Paddle.prototype.update = function() {
   this.y += this.vy;
-  this.y = constrain(this.y,0,height-this.h);
+  this.y = constrain(this.y, 0 + this.h / 2, height - this.h / 2);
 }
 
+///////// NEW /////////
+// updateScore()
+//
+// challenge: update the socre of paddle depending on winning side
+Paddle.prototype.updateScore = function() {
+  //using constrain method to prevent value overflow
+  if (ball.winningSide == "right" && this.paddleSide == "right") {
+    this.score++;
+    ball.winningSide = null;
+  }
+  if (ball.winningSide == "left" && this.paddleSide == "left") {
+    this.score++;
+    ball.winningSide = null;
+  }
+}
+// displayScore()
+//
+// display the score at left up corner or right up corner depending on the side
+//of paddle
+Paddle.prototype.displayScore = function() {
+  //push() and pop() method to store current setting
+  push();
+  fill(66, 244, 244);
+  textSize(30);
+  var gameScore = this.score;
+  if (this.paddleSide == "right") {
+    text(gameScore, 0.25 * width, 0.15 * height)
+  }
+  if (this.paddleSide == "left") {
+    text(gameScore, 0.75 * width, 0.15 * height)
+  }
+  pop();
+}
+///////// END NEW /////////
 // display()
 //
 // Draw the paddle as a rectangle on the screen
 Paddle.prototype.display = function() {
   fill(255);
-  rect(this.x,this.y,this.w,this.h);
+  rect(this.x, this.y, this.w, this.h, 20);
 }
