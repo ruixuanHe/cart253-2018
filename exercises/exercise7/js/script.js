ff09a1be14796089031d3ffdef101d75e0c230fe
Ruixuan He <ru_he@cda090ev5815w.cdaad.concordia.ca>
2018-11-15 13:27:29 -0500

    Ex:7 start exercise 7

---
+++ exercises/exercise7/js/script.js
@@ -0,0 +1,13 @@
+// setup()
+//
+// Set up the canvas, position the images, set the image mode.
+function setup() {
+
+}
+
+// draw() method
+// Moves the felt image linearly
+// Moves the clown face toward the current mouse location
+function draw() {
+
+}
