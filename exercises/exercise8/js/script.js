67572238a536e19ab0f139ee6eb85714710130c3
Ruixuan He <ru_he@cda123ev5635x.local>
2018-11-22 08:52:21 -0500

    Ex8: start exercise 8

---
+++ exercises/exercise8/js/script.js
@@ -0,0 +1,18 @@
+/*****************
+
+exercise8
+Ruixuan He 40000330
+
+******************/
+
+// setup()
+//
+// setup the canvas
+function setup() {
+}
+
+// draw()
+//
+// draw the canvas
+function draw() {
+}
