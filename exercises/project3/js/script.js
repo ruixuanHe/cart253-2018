7c0026e78dd59fa82e01a86800684e5c92297892
Ruixuan He <ru_he@cda090ev5815w.cdaad.concordia.ca>
2018-11-29 14:09:34 -0500

    P3: start project 3

---
+++ exercises/project3/js/script.js
@@ -0,0 +1,16 @@
+/*****************
+
+Project 1
+Ruixuan He 40000330
+
+******************/
+
+// setup()
+//
+// setup the canvas
+function setup() {}
+
+// draw()
+//
+// draw the canvas
+function draw() {}
