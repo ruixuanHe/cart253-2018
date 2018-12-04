7091da6418406d830c3280de07eaeee9c5510678
ruixuanHe <ruixuanheweb@gmail.com>
2018-12-04 00:32:58 -0500

    P3: create shooter object, and two shotters
    1.shooter object:
    same operation system as exercise 7, upKey and downKey to go foward and backward, left  right key change its angle.
    2. two different shooter blue and red

---
+++ exercises/project3/js/Shooter.js
@@ -0,0 +1,91 @@
+// shooter constructor
+//
+// Sets the properties with the provided arguments
+function Shooter(downKey, upKey, leftKey, rightKey, shooterSide) {
+  this.x;
+  this.y;
+  this.velocity = 5;
+  this.angle = 0;
+  this.vx = 0;
+  this.vy = 0;
+  this.score = 3;
+  this.size = 30;
+  this.color1;
+  this.color2;
+  this.color3;
+  this.color4;
+  this.downKey = downKey;
+  this.upKey = upKey;
+  this.leftKey = leftKey;
+  this.rightKey = rightKey;
+  this.shooterSide = shooterSide;
+}
+
+//setup()
+//
+//setup the color ,position and angle
+Shooter.prototype.setup = function() {
+  if (this.shooterSide === "red") {
+    this.color1 = color('#F14234');
+    this.color2 = color('#B53227');
+    this.color3 = color('#78211A');
+    this.color4 = color('#3C110D');
+    this.x = width / 4;
+    this.y = height / 2;
+  }
+  if (this.shooterSide === "blue") {
+    this.color1 = color('#3F4FFF');
+    this.color2 = color('#2F3BBF');
+    this.color3 = color('#202880');
+    this.color4 = color('#101440');
+    this.x = 3 * width / 4;
+    this.y = height / 2;
+    this.angle += 180;
+  }
+}
+
+// update()
+//
+// rotate according to its angle and move forward or backward
+Shooter.prototype.update = function() {
+  this.vy = this.velocity * sin(this.angle);
+  this.vx = this.velocity * cos(this.angle);
+  if (keyIsDown(this.upKey)) {
+    this.x += this.vx;
+    this.y += this.vy;
+  }
+  if (keyIsDown(this.downKey)) {
+    this.x -= this.vx;
+    this.y -= this.vy;
+  }
+  if (keyIsDown(this.leftKey)) {
+    this.angle -= 2.5;
+  }
+  if (keyIsDown(this.rightKey)) {
+    this.angle += 2.5;
+  }
+  this.x = constrain(this.x, 0 + this.size / 2, width - this.size / 2);
+  this.y = constrain(this.y, 0 + this.size / 2, height - this.size / 2);
+}
+
+// display()
+//
+// display shooter on screen
+Shooter.prototype.display = function() {
+  push();
+  translate(this.x, this.y);
+  //stroke(116, 127, 124);
+  fill(this.color1);
+  ellipse(0, 0, this.size + 10, this.size + 10);
+  fill(this.color2);
+  ellipse(0, 0, this.size, this.size);
+  pop();
+  push();
+  translate(this.x, this.y);
+  rotate(this.angle - 90);
+  fill(this.color3);
+  rect(0, 0, this.size / 4, this.size / 2);
+  fill(this.color4);
+  rect(0, 5 * this.size / 16, this.size / 4, this.size / 4);
+  pop();
+}
--- exercises/project3/js/script.js
+++ exercises/project3/js/script.js
@@ -1,6 +1,6 @@
 /*****************
 
-Project 1
+Project 3
 Ruixuan He 40000330
 
 ******************/
@@ -8,9 +8,25 @@ Ruixuan He 40000330
 // setup()
 //
 // setup the canvas
-function setup() {}
+function setup() {
+  background(255, 200, 255);
+  noStroke();
+  rectMode(CENTER);
+  ellipseMode(CENTER);
+  createCanvas(800, 800);
+  angleMode(DEGREES);
+  shooterRed = new Shooter(83, 87, 65, 68, "red");
+  shooterBlue = new Shooter(40, 38, 37, 39, "blue");
+  shooterRed.setup();
+  shooterBlue.setup();
+}
 
 // draw()
 //
 // draw the canvas
-function draw() {}
+function draw() {
+  shooterBlue.update();
+  shooterRed.update();
+  shooterBlue.display();
+  shooterRed.display();
+}
